f46f15440ea8c47b167e7d76f8e09a5ed8614564
karnan <manoj.shanmugam@outlook.com>
2022-04-17 22:41:02 -0500

    max poll records

--- src/main/java/com/pettyprojects/springbootkafka/Consumer.java
+++ src/main/java/com/pettyprojects/springbootkafka/Consumer.java
@@ -1,13 +1,18 @@
 package com.pettyprojects.springbootkafka;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
 
-    @KafkaListener(topics = "test_topic", groupId = "spring_consumer")
-    public void consumeMessage(String message) {
+    @KafkaListener(topics = "test_topic", groupId = "spring_consumer", properties = {
+            "max.poll.interval.ms:510",
+            ConsumerConfig.MAX_POLL_RECORDS_CONFIG + "=1"
+    })
+    public void consumeMessage(String message) throws InterruptedException {
         System.out.println(message);
+        Thread.sleep(100);
     }
 }
